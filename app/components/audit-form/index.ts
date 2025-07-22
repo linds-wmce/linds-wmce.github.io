26cdbb85b623b62b37651e7f932a31a051886869
linds-wmce <lynsey.mcelroy@phorest.com>
2025-07-22 07:28:14 +0100

    Deploy

--- app/components/audit-form/index.ts
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-
-interface AuditFormArgs {
-  htmlInput: string;
-  updateInput: (event: Event) => void;
-  runAudit: (event: Event) => void;
-}
-
-export default class AuditFormComponent extends Component<AuditFormArgs> {}
--- app/components/audit-results/index.ts
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-
-interface AuditResultsArgs {
-  results: any[];
-  htmlInput: string;
-}
-
-export default class AuditResultsComponent extends Component<AuditResultsArgs> {}
