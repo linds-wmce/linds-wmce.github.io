a24340081bdac602cc1630ac3de96c6fe0ce9eb7
linds-wmce <linden.mcelroy@phorest.com>
2025-09-24 09:14:27 +0100

    ✂️ remove custom domain

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,9 +2,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    fingerprint: {
-      prepend: 'https://linds-wmce.github.io/',
-    },
+    // fingerprint: {
+    //   prepend: 'https://linds-wmce.github.io/',
+    // },
   });
 
   return app.toTree();
